8ac278083c24355bf7d9c773d8f34be5a73227b2
isinha🫧 <isabella.r.rosa@aluno.senai.br>
2026-08-25 08:42:54 -0300

    Delete me.js

--- me.js
+++
@@ -1,9 +0,0 @@
-const { supabase, requireUser, json } = require('./_lib/supabase');
-
-module.exports = async (req, res) => {
-  try {
-    const user = await requireUser(req);
-    const { data: profile } = await supabase.from('profiles').select('*').eq('id', user.id).maybeSingle();
-    json(res, 200, { user: profile || { id:user.id, email:user.email, nome:user.email?.split('@')[0] } });
-  } catch(e) { json(res, 401, { error:e.message }); }
-};
